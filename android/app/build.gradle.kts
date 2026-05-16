7ac01c044e1d4da804160749158a7fcac8189eb0
vineeshjohnson <vineeshjohnson4545@gmail.com>
2026-05-16 15:31:12 +0530

    verified app icon and name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,47 +1,101 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
+
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
+
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+
+    // Flutter Gradle Plugin
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+
+val keystorePropertiesFile =
+    rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+
+    keystoreProperties.load(
+        FileInputStream(keystorePropertiesFile)
+    )
+}
+
 android {
+
     namespace = "com.vinish.jobtracker"
+
     compileSdk = flutter.compileSdkVersion
+
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+
         sourceCompatibility = JavaVersion.VERSION_17
+
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
+
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+
         applicationId = "com.vinish.jobtracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
+
         targetSdk = flutter.targetSdkVersion
+
         versionCode = flutter.versionCode
+
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    signingConfigs {
+
+        create("release") {
+
+            keyAlias =
+                keystoreProperties["keyAlias"] as String
+
+            keyPassword =
+                keystoreProperties["keyPassword"] as String
+
+            storeFile = file(
+                keystoreProperties["storeFile"] as String
+            )
+
+            storePassword =
+                keystoreProperties["storePassword"] as String
         }
+    }
+
+    buildTypes {
+
+     
+getByName("release") {
+
+    signingConfig =
+        signingConfigs.getByName("release")
+
+    isMinifyEnabled = false
+
+    isShrinkResources = false
+}
+
+
     }
 }
 
 flutter {
+
     source = "../.."
 }
